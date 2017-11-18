48aa102c958106fa6317e37e5b407afe400879e4
Jay Bajaj <dog.jay@gmail.com>
2017-11-18 15:22:33 -0800

    Changed to edit User info page made the nav blue and some UI fixes

--- QwkPassApp/ManageUserInfoViewController.swift
+++ QwkPassApp/ManageUserInfoViewController.swift
@@ -14,7 +14,21 @@ class ManageUserInfoViewController: UIViewController {
     @IBOutlet weak var ConfirmPassEdit: UITextField!
     @IBOutlet weak var EditPassword: UITextField!
     @IBOutlet weak var EditUserName: UITextField!
+    
+    @IBOutlet weak var EditUserNav: UINavigationBar!
+    
+    
     override func viewDidLoad() {
+        
+        EditUserNav.barTintColor = UIColor(red:0.27, green:0.56, blue:0.90, alpha:1.0)// Set any colour
+        EditUserNav.isTranslucent = false
+        
+        EditUserNav.titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:UIFont(name:"Nunito-Bold", size: 17)!]
+        
+        let barView = UIView(frame: CGRect(x:0, y:0, width:view.frame.width, height:UIApplication.shared.statusBarFrame.height))
+        barView.backgroundColor = UIColor(red:0.27, green:0.56, blue:0.90, alpha:1.0)
+        view.addSubview(barView)
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
